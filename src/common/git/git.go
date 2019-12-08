8e48eee367ecfd05922459914ceb7d19e632fd22
yycaptain <gaoyang615@163.com>
2019-12-08 10:05:23 +0800

    1

--- src/common/git/git.go
+++ src/common/git/git.go
@@ -6,8 +6,28 @@ import (
     "bytes"  
 )  
   
-func GitClone(gitUrl string ) {  
-    go worker(gitUrl)  
+type GitConfig struct {
+    GitAddress string `json:"GitAddress"`
+    WebRoot string `json:"WebRoot"`
+} 
+
+func (g *GitConfig)GitClone() {  
+    // todo existed
+   go func(){
+        command := exec.Command("git", "clone", url, g.GitAddress)  
+  
+        var bys bytes.Buffer  
+        command.Stdout = &bys  
+  
+        err := command.Start()  
+        if err != nil {  
+            panic(err)  
+        }  
+  
+        command.Wait()  
+  
+        fmt.Println(bys.String())  
+    }()
 }  
   
 func worker(url string) {  
@@ -15,7 +35,6 @@ func worker(url string) {
   
     var bys bytes.Buffer  
     command.Stdout = &bys  
-  
   
     err := command.Start()  
     if err != nil {  
