43de94f86d805af541cd11b898787e64ce1650d5
gaoyang74 <gaoyang32@jd.com>
2020-03-14 00:04:38 +0800

    beego

---
+++ go.mod
@@ -0,0 +1,5 @@
+module harbour
+
+go 1.13
+
+replace golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 => github.com/golang/crypto v0.0.0-20191011191535-87dc89f01550
--- src/main.go
+++ src/main.go
@@ -1,47 +1,7 @@
 package main
 
-import (
-	"flag"
-	"config"
-	"fmt"
-	"os"
-	"runtime"
-	"common/mylog"
-)
+import "github.com/astaxie/beego"
 
-func main() {
-
-	/**
-	 * Run it: ./harbour -c etc/harbour.json
-	 **/
-	wordPtr := flag.String("c", "./etc/harbour.json", "Harbour configuration file.")
-    flag.Parse()
-
-	/** Load Configure **/
-	config, err := config.LoadConfig(*wordPtr)
-	if err != nil {
-		fmt.Println("main", "load config failed: ", err.Error())
-		os.Exit(-1)
-		return
-	}
-
-    /** Init Log Service **/
-	if err := mylog.Init(config.MyLog.AccLogFile,config.MyLog.InfoLogFile,
-		config.MyLog.LogLevel, config.MyLog.LogMaxAge); err != nil {
-		fmt.Println("Init Log faild: ", err.Error())
-		os.Exit(-1)
-		return
-	}
-	mylog.LogInfo("Harbour Server started.")
-
-	/** Set CpuNumber **/
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	mylog.LogInfo("Using %v cpus.", runtime.NumCPU())
-
-	/** Start Service **/
-	if err := Start(config.Service); err != nil {
-		mylog.LogError("start service failed: %v", err)
-		os.Exit(-1)
-		return
-	}
+func main(){
+	beego.Run()
 }
