a595d5b69e145665b2ac87abb4cb8fb9a2bd6922
gaoyang74 <gaoyang32@jd.com>
2020-08-23 16:38:35 +0800

    C

--- src/worker/monitor.go
+++ src/worker/monitor.go
@@ -5,14 +5,15 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type XYZData struct {
-	X string
-	Y string
-	Z string
-	A string
+	X int
+	Y int
+	Z int
+	//A string
 }
 func ReadTxtFile(path string) (*[]XYZData, error) {
 	var results []XYZData
@@ -23,7 +24,10 @@ func ReadTxtFile(path string) (*[]XYZData, error) {
 	for idx,line := range lines{
 		items := strings.Split(line,"\t")
 		if len(items) >= 3 && !strings.HasPrefix(items[0],"#"){
-			results = append(results,XYZData{X:items[0],Y:items[1],Z:items[2]})
+			x,_ := strconv.Atoi(strings.Split(items[0],".")[0])
+			y,_ := strconv.Atoi(strings.Split(items[1],".")[0])
+			z,_ := strconv.Atoi(strings.Split(items[2],".")[0])
+			results = append(results,XYZData{X:x,Y:y,Z:z})
 		}
 		if idx == 10 {
 			fmt.Printf("read success: %s , %s, %s",items[0],items[1],items[2])
